f6da86b2ef31175bc96c17b965154464f1f183f4
Yeffrey Espinoza <yeffreyespinoza200@gmail.com>
2024-04-24 10:53:49 -0500

    changes for model and controlelr

--- services/users/controller.js
+++ services/users/controller.js
@@ -7,15 +7,25 @@ exports.signin = async (req, res) => {
     const findUser = await User.findOne({
       where: {
         [Op.or]: [
-          { email: req.body.email }, // Buscar por email
-          { appleId: req.body.appleId }, // Buscar por appleId
+          { email: req.body.email ?? "null" }, // Buscar por email
+          { appleId: req.body.appleId ?? "null" }, // Buscar por appleId
         ],
       },
     });
     if (findUser) {
       if (!findUser.username) {
         const username = createRandomUsername();
-        await User.update({ username }, { where: { email: req.body.email } });
+        await User.update(
+          { username },
+          {
+            where: {
+              [Op.or]: [
+                { email: req.body.email ?? "null" }, // Buscar por email
+                { appleId: req.body.appleId ?? "null" }, // Buscar por appleId
+              ],
+            },
+          }
+        );
         return res
           .status(200)
           .json({ auth: true, ...findUser.dataValues, username });
