84f061b45e5f90587fd9f062f7949ea7f087d752
Yeffrey Espinoza <yeffreyespinoza200@gmail.com>
2024-04-26 01:26:15 -0500

    now user is deleted

--- services/users/controller.js
+++ services/users/controller.js
@@ -41,6 +41,23 @@ exports.signin = async (req, res) => {
   }
 };
 
+exports.removeAccount = async (req, res) => {
+  try {
+    let emailOrAppleId = req.params.id;
+    await User.destroy({
+      where: {
+        [Op.or]: [
+          { email: emailOrAppleId ?? "null" }, // Buscar por email
+          { appleId: emailOrAppleId ?? "null" }, // Buscar por appleId
+        ],
+      },
+    });
+    res.status(200).json({ message: "User deleted" });
+  } catch (e) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
 exports.getUsers = async (req, res) => {
   try {
     const users = await User.findAll();
--- services/users/routes.js
+++ services/users/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
-const { signin, getUsers } = require("./controller.js");
+const { signin, getUsers, removeAccount } = require("./controller.js");
 const userRoutes = Router();
 userRoutes.post("/signin", signin); //signing route
+userRoutes.delete("/:id", removeAccount); //delete user route
 userRoutes.get("/", getUsers); //get all users route
 module.exports = userRoutes;
